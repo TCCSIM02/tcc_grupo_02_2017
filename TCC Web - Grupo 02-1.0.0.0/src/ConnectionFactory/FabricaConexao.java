4c6aa12e342041179b87b9427de9d028b9acd494
Nilton <Nilton@10.71.249.169>
2017-05-17 21:09:35 -0300

    Alteração na identação da Classe FabricaConexão.

--- TCC Web - Grupo 02-1.0.0.0/src/ConnectionFactory/FabricaConexao.java
+++ TCC Web - Grupo 02-1.0.0.0/src/ConnectionFactory/FabricaConexao.java
@@ -8,18 +8,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
-	public class FabricaConexao {
-		static {
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				throw new RuntimeException(e);
-			}
+public class FabricaConexao {
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
 		}
+	}
 
-		// Obtem conexao com o banco de dados
-		// Alterar para O TCC
-		public static Connection getConexao() throws SQLException {
-			return DriverManager.getConnection("jdbc:mysql://127.0.0.1/TCC?user=alunos&password=alunos");
-		}
-	}	
+	// Obtem conexao com o banco de dados
+	// Alterar para O TCC
+	public static Connection getConexao() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://127.0.0.1/TCC?user=alunos&password=alunos");
+	}
+}	
