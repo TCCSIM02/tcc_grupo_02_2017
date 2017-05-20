76547038645365ba3ad5bc73aeed8be4fa6208cb
Nilton <Nilton@Nilton-PC>
2017-05-20 12:03:18 -0300

    Criação classes Command e ServeletController, ainda falta o tomcat.

---
+++ TCC Web - Grupo 02-1.0.0.0/src/DAO/DAOPlano.java
@@ -0,0 +1,20 @@
+package DAO;
+
+import ConnectionFactory.FabricaConexao;
+
+import TO.TOPlano;
+
+
+import java.sql.Connection; 
+import java.sql.PreparedStatement; 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList; 
+
+public class DAOPlano {
+
+	public void inserirPlano(TOPlano toPlano){
+		
+	}
+	
+}
