ee58b029c00cda74f6b60ad4a91da8e383b9d809
Nilton <niltonfilho95@gmail.com>
2017-09-08 12:28:50 -0300

    Classes de Login criadas

---
+++ TCC Web - Grupo 02-1.0.0.0/src/TO/TOLogin.java
@@ -0,0 +1,84 @@
+package TO;
+
+import java.util.Date;
+
+public class TOLogin {
+
+	public int codLogin;
+	public String nomeLogin, email, senha, flagAtivo;
+	public Date dataCadastro;
+	/**
+	 * @return the codLogin
+	 */
+	public int getCodLogin() {
+		return codLogin;
+	}
+	/**
+	 * @return the nomeLogin
+	 */
+	public String getNomeLogin() {
+		return nomeLogin;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
+	/**
+	 * @return the flagAtivo
+	 */
+	public String getFlagAtivo() {
+		return flagAtivo;
+	}
+	/**
+	 * @return the dataCadastro
+	 */
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	/**
+	 * @param codLogin the codLogin to set
+	 */
+	public void setCodLogin(int codLogin) {
+		this.codLogin = codLogin;
+	}
+	/**
+	 * @param nomeLogin the nomeLogin to set
+	 */
+	public void setNomeLogin(String nomeLogin) {
+		this.nomeLogin = nomeLogin;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @param senha the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	/**
+	 * @param flagAtivo the flagAtivo to set
+	 */
+	public void setFlagAtivo(String flagAtivo) {
+		this.flagAtivo = flagAtivo;
+	}
+	/**
+	 * @param dataCadastro the dataCadastro to set
+	 */
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+	
+}
