f13cfc149a5a7df08344ba1c0da7b26e62bc6242
Nilton <niltonfilho95@gmail.com>
2017-09-30 15:16:20 -0300

    Atualizacao para conectar banco de dados na nuvem, AWS.

--- TCC Web - Grupo 02-1.0.0.0/src/ConnectionFactory/FabricaConexao.java
+++ TCC Web - Grupo 02-1.0.0.0/src/ConnectionFactory/FabricaConexao.java
@@ -18,6 +18,12 @@ public class FabricaConexao {
 	// Obtem conexao com o banco de dados
 	// Alterar para O TCC
 	public static Connection getConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://127.0.0.1/TCC?user=alunos&password=alunos");
+		//return DriverManager.getConnection("jdbc:mysql://127.0.0.1/TCC?user=alunos&password=alunos");
+		return DriverManager.getConnection("jdbc:mysql://tccdbawsintance.capgtveukqse.sa-east-1.rds.amazonaws.com:3306/TCC?user=alunos&password=alunos_tcc");
+		
+		/*	Database name: tcc
+		 * 	Database port: 3306
+		 * 	Endpoint: tccdbawsintance.capgtveukqse.sa-east-1.rds.amazonaws.com
+		 * */
 	}
 }	
